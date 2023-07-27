034a5ef8f11049a3185ec69f22d0015c702951ce
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-27 11:28:41 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-mongodbatlas-network-peering/index.ts
+++ src/data-mongodbatlas-network-peering/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.10.2/docs/data-sources/network_peering
 // generated from terraform resource schema
 
