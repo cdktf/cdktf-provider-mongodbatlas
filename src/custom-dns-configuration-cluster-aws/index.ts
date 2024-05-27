6f8b0dd297288b0005f85a40647fddaf8add41f4
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 08:59:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/custom-dns-configuration-cluster-aws/index.ts
+++ src/custom-dns-configuration-cluster-aws/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.16.0/docs/resources/custom_dns_configuration_cluster_aws
 // generated from terraform resource schema
 
