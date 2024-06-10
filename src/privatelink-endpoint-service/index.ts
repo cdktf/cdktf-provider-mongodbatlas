c00f21bb181f6bb141fbe7688bac54ac154688c7
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-10 08:06:14 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/privatelink-endpoint-service/index.ts
+++ src/privatelink-endpoint-service/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.16.2/docs/resources/privatelink_endpoint_service
 // generated from terraform resource schema
 
