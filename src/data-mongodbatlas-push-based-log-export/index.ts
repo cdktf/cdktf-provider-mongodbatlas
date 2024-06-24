8ab678a950859c28a5d0d519f8764a7c641faee2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-24 12:36:57 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-mongodbatlas-push-based-log-export/index.ts
+++ src/data-mongodbatlas-push-based-log-export/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.17.2/docs/data-sources/push_based_log_export
 // generated from terraform resource schema
 
