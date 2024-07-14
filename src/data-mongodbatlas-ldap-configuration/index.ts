b7a961a7c0f83e36517b254ff9868a2ab8ec235d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-14 06:08:12 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@67b9ba0

--- src/data-mongodbatlas-ldap-configuration/index.ts
+++ src/data-mongodbatlas-ldap-configuration/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.17.3/docs/data-sources/ldap_configuration
 // generated from terraform resource schema
 
