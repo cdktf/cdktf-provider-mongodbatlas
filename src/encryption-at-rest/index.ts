6d7f5404eaf55bf06e2ea1f6456205c555aaf550
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-21 06:06:17 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@30ba874

--- src/encryption-at-rest/index.ts
+++ src/encryption-at-rest/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.17.4/docs/resources/encryption_at_rest
 // generated from terraform resource schema
 
