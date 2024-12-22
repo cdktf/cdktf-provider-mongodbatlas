7e704ccdcc6e95506fb1a4a1c1ef232f33a252cf
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-22 06:10:25 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-mongodbatlas-federated-database-instances/index.ts
+++ src/data-mongodbatlas-federated-database-instances/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.24.0/docs/data-sources/federated_database_instances
 // generated from terraform resource schema
 
