efa07b89524d952edc5720ce0a8457f999a4950e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-31 13:25:41 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@edbf1a8

--- src/data-mongodbatlas-flex-cluster/index.ts
+++ src/data-mongodbatlas-flex-cluster/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.31.0/docs/data-sources/flex_cluster
 // generated from terraform resource schema
 
