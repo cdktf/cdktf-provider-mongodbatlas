1b795984fc053e11f14129b878d65d0a1f64e722
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-31 13:28:29 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/encryption-at-rest-private-endpoint/index.ts
+++ src/encryption-at-rest-private-endpoint/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.31.0/docs/resources/encryption_at_rest_private_endpoint
 // generated from terraform resource schema
 
