4c1380f7396ea172b502b52da92ff935ab428c4c
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:50:59 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/encryption-at-rest-private-endpoint/index.ts
+++ src/encryption-at-rest-private-endpoint/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/mongodb/mongodbatlas/1.35.1/docs/resources/encryption_at_rest_private_endpoint
 // generated from terraform resource schema
 
